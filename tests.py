131034d1fbde1e3d16a78a029640a2763d5cda16
lucas-ht <46166712+lucas-ht@users.noreply.github.com>
2024-05-04 16:46:45 +0200

    Fix test suite

--- tests.py
+++ tests.py
@@ -2,14 +2,21 @@
 This module is used to run all the tests in the tests directory.
 """
 
+import sys
 import unittest
+
 
 def main() -> None:
     """
     The main function for running the tests.
     """
+
     tests = unittest.TestLoader().discover('tests', pattern='test_*.py')
-    unittest.TextTestRunner().run(tests)
+    result = unittest.TextTestRunner().run(tests)
+
+    if not result.wasSuccessful():
+        sys.exit(1)
+
 
 if __name__ == '__main__':
     main()
